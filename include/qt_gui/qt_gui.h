21545d08297e31d424e106e5f156343efb8bf5dd
BorderArea01 <3109362852@qq.com>
2024-11-07 18:57:14 +0800

    封装弹窗功能；调整自适应布局；暂时延后无边框布局的实现

--- include/qt_gui/qt_gui.h
+++ include/qt_gui/qt_gui.h
@@ -1,65 +1,75 @@
 #pragma once
-//#pragma once 是一种防止头文件被多次包含的方式。
+#include <qlabel.h>
+#include <qmainwindow.h>
 #include <qpixmap.h>
+#include <qpushbutton.h>
 #include <qwidget.h>
+
+#include <QAction>
+#include <QCoreApplication>
 #include <QGridLayout>
-//#include <QToolBar>
-#include <QLabel>
-#include <QTextEdit>
-#include <QLineEdit>
 #include <QGroupBox>
-#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
 #include <QMainWindow>
-//Qt 库中的图像处理、窗口和布局类。
+#include <QMenuBar>
+#include <QPushButton>
+#include <QTextEdit>
+
 #include <opencv2/core/mat.hpp>
-//OpenCV 中的 cv::Mat，用于表示图像数据。
+
 #include "camera.h"
 #include "face_analyzer.h"
 #include "observer/core.h"
-//自定义的 camera.h 和 face_analyzer.h 头文件。
-//observer/core.h 可能与观察者模式有关，用于事件通知。
+#include "qt_gui/notification.h"
 
-namespace arm_face_id { //定义一个命名空间 arm_face_id，用于组织代码和避免命名冲突。
-class QtGUI //QtGUI 继承自 QMainWindow，这是一个 Qt 的主窗口类。
+namespace arm_face_id {
+class QtGUI
     : public QMainWindow,
       public treasure_chest::pattern::Observer<cv::Mat>,
-      public treasure_chest::pattern::Observer<FaceAnalyzer::EventBase> {//实现了两个观察者接口，用于响应 cv::Mat 和 FaceAnalyzer::EventBase 类型的事件。
-
+      public treasure_chest::pattern::Observer<FaceAnalyzer::EventBase> {
  public:
-  QtGUI() = default;//默认构造函数，未进行任何特殊初始化。
-  void InitWindow();//InitWindow() 方法用于初始化窗口的设置，具体实现不在此处。
-//  void retranslateUi(QMainWindow *MainWindow);
-  void OnNotify(const cv::Mat& message) override;
-  void OnNotify(const FaceAnalyzer::EventBase& message) override;//OnNotify 方法是观察者接口的实现，用于处理通知事件，分别针对图像数据和人脸分析事件。
+  QtGUI() = default;
+  void InitWindow();
+  void OnNotify(const cv::Mat &message) override;
+  void OnNotify(const FaceAnalyzer::EventBase &message) override;
 
  private:
-  QWidget* centralwidget = nullptr;//主窗口的 QWidget。
-  QGridLayout* main_grid_layout_ = nullptr;//用于布局的网格布局。
-//  QLabel* camera_frame_label_ = nullptr;//显示摄像头帧的标签。
-  QPixmap info_notification_pixmap_;//用于显示信息通知的图像。
+  QWidget *centralwidget = nullptr;
+  QGridLayout *main_grid_layout_ = nullptr;
 
-//  QMainWindow *MainWindow = nullptr; //主窗口
+  // 用于显示信息通知的图像。
+  QPixmap info_notification_pixmap_;
+
+  // 图像资源
   QLabel *lb_bg = nullptr;
   QLabel *lb_ico = nullptr;
-  QTabWidget *tw_main = nullptr;
-  QWidget *tab_func = nullptr;
-  QGroupBox *bg_camera = nullptr;
-  QLabel *lb_camera = nullptr;//显示摄像头帧的标签。
+
+  // menu 栏
+  QMenuBar *menubar;
+  QMenu *menu_0, *menu_1;
+  QAction *menu_0_action_0, *menu_0_action_1, *menu_1_action_0;
+
+  // cam 栏
+  QVBoxLayout *cam_vbox_layout = nullptr;
+  QGroupBox *gb_camera = nullptr;
+  QLabel *lb_camera = nullptr;
   QGroupBox *gb_info = nullptr;
-  QGridLayout *gridLayout = nullptr;
-  QLabel *lb_email = nullptr;
-  QLineEdit *le_lastused = nullptr;
-  QLabel *lb_facequality = nullptr;
+
+  // info 栏
+  QGridLayout *info_grid_layout = nullptr;
+  QLabel *lb_headshot = nullptr;
   QLabel *lb_username = nullptr;
-  QLineEdit *le_facequality = nullptr;
   QLineEdit *le_username = nullptr;
-  QLabel *lb_lastused = nullptr;
+  QLabel *lb_email = nullptr;
   QLineEdit *le_email = nullptr;
-  QLabel *lb_liveness = nullptr;
-  QLineEdit *le_liveness = nullptr;
-  QWidget *tab_log = nullptr;
-  QWidget *tab_about = nullptr;
-  QLabel *lb_about = nullptr;
+  QLabel *lb_lastused = nullptr;
+  QLineEdit *le_lastused = nullptr;
+  QPushButton *pb_verify1 = nullptr;
+  QPushButton *pb_verify2 = nullptr;
+  Notification *nf1, *nf2;
+
+  // 关闭|最小化按钮
   QPushButton *pb_close = nullptr;
   QPushButton *pb_hide = nullptr;
 };
--- src/qt_gui/qt_gui.cpp
+++ src/qt_gui/qt_gui.cpp
@@ -1,3 +1,6 @@
+#include "qt_gui/qt_gui.h"
+
+#include <qcoreapplication.h>
 #include <qgridlayout.h>
 #include <qicon.h>
 #include <qlabel.h>
@@ -9,252 +12,189 @@
 #include <qwidget.h>
 
 #include <string>
-// 包含了必要的 Qt 组件
-#include "face_analyzer.h"
-#include "qt_gui/qt_gui.h"
-#include "utils/base.h"
-// 包含了自定义头文件，准备构建 GUI。
 
+#include "face_analyzer.h"
+#include "qt_gui/notification.h"
+#include "utils/base.h"
 using namespace arm_face_id;
-// 引入 arm_face_id
-// 命名空间，以便在后续代码中可以直接使用该命名空间下的类和函数。
 
 void QtGUI::InitWindow() {
   centralwidget = new QWidget;
   setCentralWidget(centralwidget);
-  // 创建一个 QWidget 作为主窗口，并将其设置为中心控件。
   main_grid_layout_ = new QGridLayout(centralwidget);
-  //  camera_frame_label_ = new QLabel("camera");
-  //  camera_frame_label_->setFixedSize(640, 400);
-  // 创建一个标签，用于显示摄像头画面，并设置其固定大小。
-  //**************************新UI设计******************************//
-  lb_bg = new QLabel(centralwidget);
-  lb_bg->setObjectName("lb_bg");
-  lb_bg->setGeometry(QRect(-20, -10, 951, 631));
-  lb_bg->setAutoFillBackground(false);
-  lb_bg->setPixmap(QPixmap(QString::fromUtf8("assets/bg.png")));
-  lb_bg->setScaledContents(true);
-  lb_ico = new QLabel(centralwidget);
-  lb_ico->setObjectName("lb_ico");
-  lb_ico->setGeometry(QRect(30, 30, 71, 61));
-  lb_ico->setPixmap(QPixmap(QString::fromUtf8("assets/fr2.ico")));
-  tw_main = new QTabWidget(centralwidget);
-  tw_main->setObjectName("tw_main");
-  tw_main->setGeometry(QRect(100, 40, 771, 541));
-  QFont font;
-  font.setFamilies(
-      {QString::fromUtf8("\345\276\256\350\275\257\351\233\205\351\273\221")});
-  font.setPointSize(14);
-  tw_main->setFont(font);
-  tw_main->setAutoFillBackground(false);
-  tw_main->setStyleSheet(
-      QString::fromUtf8("QTabWidget::pane{\n"
-                        "min-width:70px;\n"
-                        "min-height:25px;\n"
-                        "border-top: 2px solid;}\n"
-                        "QTabBar::tab {\n"
-                        "min-width:70px;\n"
-                        "min-height:25px;\n"
-                        "color: white;\n"
-                        "font:16px \"Microsoft YaHei\";\n"
-                        "border: 0px solid;}\n"
-                        "QTabBar::tab:selected{\n"
-                        "min-width:70px;\n"
-                        "min-height:45px;\n"
-                        "color: #4796f0;\n"
-                        "font:18px \"Microsoft YaHei\";\n"
-                        "border: 0px solid;\n"
-                        "border-bottom: 3px solid;\n"
-                        "border-color: #4796f0;}"));
-  tw_main->setTabsClosable(false);
-  tw_main->setMovable(false);
-  tw_main->setTabBarAutoHide(false);
-  tab_func = new QWidget();
-  tab_func->setObjectName("tab_func");
-  bg_camera = new QGroupBox(tab_func);
-  bg_camera->setObjectName("bg_camera");
-  // bg_camera->setGeometry(QRect(10, 10, 521, 471));
-  bg_camera->setStyleSheet(QString::fromUtf8("color:white"));
 
-  // add layout for bg
-  QVBoxLayout* gb_camera_layout = new QVBoxLayout(bg_camera);
-  gb_camera_layout->setContentsMargins(10, 10, 10, 10);
+  // 设置大小策略
+  QSizePolicy sizePolicy(QSizePolicy::Policy::Expanding,
+                         QSizePolicy::Policy::Expanding);
+  centralwidget->setSizePolicy(sizePolicy);
+  centralwidget->setMinimumSize(400, 200);
 
+  centralwidget->setStyleSheet(QString::fromUtf8("background-color:#272b4c"));
+  // // 设置背景图片
+  // lb_bg = new QLabel(centralwidget);
+  // main_grid_layout_->addWidget(lb_bg, 0, 0, 1, 2);
+  // lb_bg->setSizePolicy(sizePolicy);
+  // lb_bg->setPixmap(QPixmap(QString::fromUtf8("assets/bg.png")));
+  // lb_bg->setScaledContents(true);
+  // lb_bg->setMinimumSize(380, 180);
+
+  // menu栏
+  // menubar = new QMenuBar(centralwidget);
+  // menubar->setObjectName("menubar");
+  // // menubar->setGeometry(QRect(0, 0, 863, 33));
+  // menu_0 = new QMenu(menubar);
+  // menu_0->setObjectName("menu");
+  // menu_1 = new QMenu(menubar);
+  // menu_1->setObjectName("menutool");
+  // // centralwidget->setMenuBar(menubar);
+  // // statusbar = new QStatusBar(centralwidget);
+  // // statusbar->setObjectName("statusbar");
+  // // centralwidget->setStatusBar(statusbar);
+  // menubar->addAction(menu_0->menuAction());
+  // menu_0->addAction(menu_0_action_0);
+  // menu_0->addAction(menu_0_action_1);
+  // menubar->addAction(menu_1->menuAction());
+  // menu_1->addAction(menu_1_action_0);
+
+  // cam 栏目
+  gb_camera = new QGroupBox(lb_bg);
+  main_grid_layout_->addWidget(gb_camera, 0, 0);
+  main_grid_layout_->setColumnStretch(0, 2);
+  cam_vbox_layout = new QVBoxLayout(gb_camera);
+  // cam_vbox_layout->setContentsMargins(10, 10, 10, 10);
+  // cam 幕布
   lb_camera = new QLabel;
-  lb_camera->setObjectName("lb_camera");
-  // lb_camera->setGeometry(QRect(10, 20, 491, 431));
-  // 创建一个与标签同样大小的 QPixmap，并填充为透明色，后续将用于叠加信息提示。
-  QPixmap pixmap(lb_camera->size());
-  QPainter painter(&pixmap);
-  painter.setBrush(Qt::blue);
-  painter.drawRect(0, 0, pixmap.width(), pixmap.height());
-  lb_camera->setPixmap(pixmap);
-  gb_camera_layout->addWidget(lb_camera);
-
-  // lb_camera = new QLabel;
-  // lb_camera->setObjectName("lb_camera1");
-  // lb_camera->setGeometry(QRect(10, 20, 491, 431));
-  // // 创建一个与标签同样大小的
-  // QPixmap，并填充为透明色，后续将用于叠加信息提示。 QPixmap
-  // pixmap1(lb_camera->size()); QPainter painter1(&pixmap);
-  // painter.setBrush(Qt::yellow);
-  // painter.drawRect(0, 0, pixmap.width(), pixmap.height());
-  // lb_camera->setPixmap(pixmap);
-  // gb_camera_layout->addWidget(lb_camera);
-
-  gb_info = new QGroupBox(tab_func);
-  gb_info->setObjectName("gb_info");
-  gb_info->setGeometry(QRect(540, 30, 211, 431));
-  gb_info->setStyleSheet(QString::fromUtf8("color:white"));
-  gridLayout = new QGridLayout(gb_info);
-  gridLayout->setObjectName("gridLayout");
-  lb_email = new QLabel(gb_info);
-  lb_email->setObjectName("lb_email");
-
-  gridLayout->addWidget(lb_email, 4, 1, 1, 1);
-
-  le_lastused = new QLineEdit(gb_info);
-  le_lastused->setObjectName("le_lastused");
-  le_lastused->setReadOnly(true);
-
-  gridLayout->addWidget(le_lastused, 5, 2, 1, 1);
-
-  lb_facequality = new QLabel(gb_info);
-  lb_facequality->setObjectName("lb_facequality");
-
-  gridLayout->addWidget(lb_facequality, 1, 1, 1, 1);
-
-  lb_username = new QLabel(gb_info);
-  lb_username->setObjectName("lb_username");
-
-  gridLayout->addWidget(lb_username, 3, 1, 1, 1);
-
-  le_facequality = new QLineEdit(gb_info);
-  le_facequality->setObjectName("le_facequality");
-  le_facequality->setReadOnly(true);
-
-  gridLayout->addWidget(le_facequality, 1, 2, 1, 1);
-
-  le_username = new QLineEdit(gb_info);
-  le_username->setObjectName("le_username");
-  le_username->setReadOnly(true);
-
-  gridLayout->addWidget(le_username, 3, 2, 1, 1);
-
-  lb_lastused = new QLabel(gb_info);
-  lb_lastused->setObjectName("lb_lastused");
-
-  gridLayout->addWidget(lb_lastused, 5, 1, 1, 1);
-
-  le_email = new QLineEdit(gb_info);
-  le_email->setObjectName("le_email");
-  le_email->setReadOnly(true);
-
-  gridLayout->addWidget(le_email, 4, 2, 1, 1);
-
-  lb_liveness = new QLabel(gb_info);
-  lb_liveness->setObjectName("lb_liveness");
-
-  gridLayout->addWidget(lb_liveness, 2, 1, 1, 1);
-
-  le_liveness = new QLineEdit(gb_info);
-  le_liveness->setObjectName("le_liveness");
-  le_liveness->setReadOnly(true);
-
-  gridLayout->addWidget(le_liveness, 2, 2, 1, 1);
-
-  tw_main->addTab(tab_func, QString());
-  tab_log = new QWidget();
-  tab_log->setObjectName("tab_log");
-  tw_main->addTab(tab_log, QString());
-  tab_about = new QWidget();
-  tab_about->setObjectName("tab_about");
-  lb_about = new QLabel(tab_about);
-  lb_about->setObjectName("lb_about");
-  lb_about->setGeometry(QRect(30, 70, 711, 411));
-  lb_about->setStyleSheet(QString::fromUtf8("color:white"));
-  lb_about->setTextFormat(Qt::TextFormat::AutoText);
-  lb_about->setAlignment(Qt::AlignmentFlag::AlignLeading |
-                         Qt::AlignmentFlag::AlignLeft |
-                         Qt::AlignmentFlag::AlignTop);
-  lb_about->setWordWrap(true);
-  tw_main->addTab(tab_about, QString());
-  pb_close = new QPushButton(centralwidget);
-  pb_close->setObjectName("pb_close");
-  pb_close->setGeometry(QRect(860, 50, 20, 21));
-  pb_close->setLayoutDirection(Qt::LayoutDirection::LeftToRight);
-  pb_close->setStyleSheet(QString::fromUtf8(
-      "QPushButton{\n"
-      "    background:#CE0000;\n"
-      "    color:white;\n"
-      "    box-shadow: 1px 1px 3px rgba(0,0,0,0.3);\n"
-      "	border-radius: 8px;}\n"
-      "QPushButton:hover{background:#FF1493;}\n"
-      "QPushButton:pressed{border: 1px solid #3C3C3C!important;}"));
-  pb_hide = new QPushButton(centralwidget);
-  pb_hide->setObjectName("pb_hide");
-  pb_hide->setGeometry(QRect(831, 50, 20, 21));
-  pb_hide->setStyleSheet(QString::fromUtf8(
-      "QPushButton{\n"
-      "    background:#96d8ff;\n"
-      "    color:white;\n"
-      "    box-shadow: 1px 1px 3px rgba(0,0,0,0.3);\n"
-      "	border-radius: 8px;}\n"
-      "QPushButton:hover{background:#7FFFD4;}\n"
-      "QPushButton:pressed{border: 1px solid #3C3C3C!important;}"));
-  setCentralWidget(centralwidget);
-  //        retranslateUi(MainWindow);
-  tw_main->setCurrentIndex(0);
-  //        QMetaObject::connectSlotsByName(MainWindow);
-
-  //***************************************************************//
+  cam_vbox_layout->addWidget(lb_camera);
+  QPixmap pixmap_cam(lb_camera->size());
+  QPainter painter_cam(&pixmap_cam);
+  painter_cam.setBrush(Qt::blue);
+  painter_cam.drawRect(0, 0, pixmap_cam.width(), pixmap_cam.height());
+  lb_camera->setPixmap(pixmap_cam);
   info_notification_pixmap_ = QPixmap(lb_camera->size());
   info_notification_pixmap_.fill(Qt::transparent);
 
-  // 创建一个黑色背景的 QPixmap，并将其设置为标签的显示内容。
-  //   main_grid_layout_->addWidget(toolbar);
-  //// 将 toolbar_frame_label_ 添加到布局中，放在网格的第一行第一列。
-  //    main_grid_layout_->addWidget(info_text_edit, 1, 1);
-  //// 将 info_frame_label_ 添加到布局中，放在网格的第二行第二列。'
-  main_grid_layout_->addWidget(lb_camera, 1, 0);
-  // 将 camera_frame_label_ 添加到布局中，放在网格的第二行第一列。
+  // info 栏目
+  gb_info = new QGroupBox(lb_bg);
+  main_grid_layout_->addWidget(gb_info, 0, 1);
+  main_grid_layout_->setColumnStretch(1, 1);
+  gb_info->setStyleSheet(QString::fromUtf8("color:white"));
+  info_grid_layout = new QGridLayout(gb_info);
+  // 显示用户个性头像
+  lb_headshot = new QLabel(gb_info);
+  info_grid_layout->addWidget(lb_headshot, 0, 0, 2, 2);
+  info_grid_layout->setColumnStretch(0, 2);
+  info_grid_layout->setRowStretch(0, 2);
+  QPixmap pixmap_hs(lb_headshot->size());
+  QPainter painter_hs(&pixmap_hs);
+  painter_hs.setBrush(Qt::red);
+  painter_hs.drawRect(0, 0, pixmap_hs.width(), pixmap_hs.height());
+  lb_headshot->setPixmap(pixmap_hs);
+  // 显示用户昵称
+  lb_username = new QLabel(gb_info);
+  info_grid_layout->addWidget(lb_username, 3, 1, 1, 1);
+  le_username = new QLineEdit(gb_info);
+  le_username->setReadOnly(true);
+  info_grid_layout->addWidget(le_username, 3, 2, 1, 1);
+  // 显示电子邮箱
+  lb_email = new QLabel(gb_info);
+  info_grid_layout->addWidget(lb_email, 4, 1, 1, 1);
+  le_email = new QLineEdit(gb_info);
+  le_email->setReadOnly(true);
+  info_grid_layout->addWidget(le_email, 4, 2, 1, 1);
+  // 显示上一次使用时间
+  lb_lastused = new QLabel(gb_info);
+  info_grid_layout->addWidget(lb_lastused, 5, 1, 1, 1);
+  le_lastused = new QLineEdit(gb_info);
+  le_lastused->setReadOnly(true);
+  info_grid_layout->addWidget(le_lastused, 5, 2, 1, 1);
+  // 测试用按钮，点击弹出 人脸质量检测 结果
+  pb_verify1 = new QPushButton("FunctionVerify1", gb_info);
+  info_grid_layout->addWidget(pb_verify1, 6, 1, 1, 2);
+  // 测试用按钮，点击弹出 活体检测 结果
+  pb_verify2 = new QPushButton("FunctionVerify2", gb_info);
+  info_grid_layout->addWidget(pb_verify2, 7, 1, 1, 2);
 
-  resize(936, 611);
+  // // 关闭按钮
+  // pb_close = new QPushButton(centralwidget);
+  // pb_close->setLayoutDirection(Qt::LayoutDirection::LeftToRight);
+  // pb_close->setStyleSheet(QString::fromUtf8(
+  //     "QPushButton{\n"
+  //     "    background:#CE0000;\n"
+  //     "    box-shadow: 1px 1px 3px rgba(0,0,0,0.3);\n"
+  //     "	border-radius: 8px;}\n"
+  //     "QPushButton:hover{background:#FF1493;}\n"
+  //     "QPushButton:pressed{border: 1px solid #3C3C3C!important;}"));
+
+  // // 最小化按钮
+  // pb_hide = new QPushButton(centralwidget);
+  // pb_hide->setStyleSheet(QString::fromUtf8(
+  //     "QPushButton{\n"
+  //     "    background:#96d8ff;\n"
+  //     "    box-shadow: 1px 1px 3px rgba(0,0,0,0.3);\n"
+  //     "	border-radius: 8px;}\n"
+  //     "QPushButton:hover{background:#7FFFD4;}\n"
+  //     "QPushButton:pressed{border: 1px solid #3C3C3C!important;}"));
+
+  // 赋名
+  gb_info->setTitle(
+      QCoreApplication::translate("MainWindow", "Information", nullptr));
+  lb_username->setText(
+      QCoreApplication::translate("MainWindow", "Username", nullptr));
+  lb_email->setText(
+      QCoreApplication::translate("MainWindow", "Email", nullptr));
+  lb_lastused->setText(
+      QCoreApplication::translate("MainWindow", "LastUsed", nullptr));
+  //   pb_verify->setText(
+  //       QCoreApplication::translate("MainWindow", "FunctionVerify",
+  //       nullptr));
+
+  // menu_0->setTitle(QCoreApplication::translate("MainWindow", "menu0",
+  // nullptr)); menu_1->setTitle(QCoreApplication::translate("MainWindow",
+  // "menu1", nullptr)); menu_0_action_0->setText(
+  //     QCoreApplication::translate("MainWindow", "Start", nullptr));
+  // menu_0_action_1->setText(
+  //     QCoreApplication::translate("MainWindow", "Stop", nullptr));
+  // menu_1_action_0->setText(
+  //     QCoreApplication::translate("MainWindow", "...", nullptr));
+
+  // 测试按钮
+  nf1 = new Notification(centralwidget);
+  connect(pb_verify1, &QPushButton::clicked, this, [&]() {
+    nf1->Push(NotifyType::Notify_Type_Warning, NotifyPosition::Pos_Top_Left,
+              "Warning", "The face-quality-detection failed!", 1000);
+  });
+  pb_verify1->move(200, 50);
+  nf2 = new Notification(centralwidget);
+  connect(pb_verify2, &QPushButton::clicked, this, [&]() {
+    nf2->Push(NotifyType::Notify_Type_Error, NotifyPosition::Pos_Top_Left,
+              "Error", "The liveness-detection failed!", 1000);
+  });
+  pb_verify2->move(200, 50);
 }
 
 void QtGUI::OnNotify(const cv::Mat& message) {
-  // 将信息提示帧与画面帧叠加
   QPixmap frame = QPixmap::fromImage(utils::mat_to_qimage(message));
-  // 将 cv::Mat 类型的图像转换为 QPixmap。
   QPainter painter(&frame);
   painter.drawPixmap(0, 0, info_notification_pixmap_);
   lb_camera->setPixmap(frame);
-}  // 使用 QPainter
-   // 在frame上绘制之前准备的透明信息提示，最后更新标签的显示内容。
+}
 
 void QtGUI::OnNotify(const FaceAnalyzer::EventBase& message) {
-  // 通过 EventBase 的 type 转换为具体的信息类型
   if (message.type == FaceAnalyzer::DETECTOR) {
     const auto& event =
         static_cast<const FaceAnalyzer::DetectorEvent&>(message);
-    // 检查事件类型是否为人脸检测，如果是，则将事件转换为具体的DetectorEvent类型。
-    //  绘制人脸检测框
-    //  QPixmap pixmap = camera_frame_label_->pixmap();
-    //  绘制并缓存信息提示帧，待与画面帧叠加，避免丢失信息提示帧
     info_notification_pixmap_.fill(Qt::transparent);
     QPainter painter(&info_notification_pixmap_);
     painter.setPen(Qt::yellow);
-    // 清空之前的提示图层，并准备绘制新的人脸检测框，设置画笔颜色为黄色。
     for (int i = 0; i < event.faces.size; i++) {
       auto& face = event.faces.data[i];
       auto& face_rect = event.faces.data[i].pos;
       painter.drawRect(face_rect.x, face_rect.y, face_rect.width,
                        face_rect.height);
-
       painter.drawText(face_rect.x + 5, face_rect.y + 15,
                        "code：" + QString::number(i));
       painter.drawText(face_rect.x + 5, face_rect.y + face_rect.height - 5,
                        "score：" + QString::number(face.score));
-    }  // 遍历检测到的人脸，绘制每个人脸的矩形框和相关信息（编号和分数）。
+    }
   }
 }
